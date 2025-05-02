30c6771faec46d521621c218dbc2372d2831dc5a
dmicsilva <dmic.silva@campus.fct.unl.pt>
2025-05-02 14:48:43 +0100

    Addition of medPalmTest.py

---
+++ scripts/medPalmTest.py
@@ -0,0 +1,12 @@
+import torch
+from medpalm.model import MedPalm
+
+#usage
+img = torch.randn(1, 3, 256, 256)
+text = torch.randint(0, 20000, (1, 4096))
+
+model = MedPalm()
+output = model(img, text)
+print(output.shape)
+
+#https://github.com/kyegomez/Med-PaLM
