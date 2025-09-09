005ab366ce7c6b24d1d8867d39e6bdcad5554ea8
dmicsilva <dmic.silva@campus.fct.unl.pt>
2025-09-09 02:30:42 +0100

    mammogram dataset prepping done

---
+++ datasets/datasetPrepping/mammogramDatasetPrepping.py
@@ -0,0 +1,28 @@
+import os
+
+cwd = os.getcwd()
+
+src_folder = 'datasets/breast-cancer-detection-mammogram'
+
+dest_healthy = os.path.join(cwd, 'mammogramDataset', 'healthy')
+dest_sick = os.path.join(cwd, 'mammogramDataset', 'sick')
+
+os.makedirs(dest_healthy, exist_ok=True)
+os.makedirs(dest_sick, exist_ok=True)
+
+subfolders_to_process = ['test', 'valid']
+sub_subfolders_to_process = ['0', '1']
+
+for subfolder in subfolders_to_process:
+    for sub_subfolder in sub_subfolders_to_process:
+        src_path = os.path.join(src_folder, subfolder, sub_subfolder)
+        
+        for filename in os.listdir(src_path):
+            src_file = os.path.join(src_path, filename)
+            
+            dest_filename = f"{os.path.splitext(filename)[0]}{'_healthy' if sub_subfolder == '0' else '_sick'}{os.path.splitext(filename)[1]}"
+            dest_file = os.path.join(dest_sick if sub_subfolder == '1' else dest_healthy, dest_filename)
+
+            with open(src_file, 'rb') as src_f:
+                with open(dest_file, 'wb') as dest_f:
+                    dest_f.write(src_f.read())
--- scripts/fileInteraction.py
+++ scripts/fileInteraction.py
@@ -2,20 +2,23 @@ import json
 from PIL import Image
 import base64
 import os
+import cv2
 
 def encode_image_to_base64(image_path):
-    """Convert an image file to base64 string."""
-    return base64.b64encode(image_path.read_bytes()).decode('utf-8')
+    
+    if image_path.endswith(('.tif', '.tiff')):
+        img = cv2.imread(image_path)
+        return base64_string = base64.b64encode(cv2.imencode('.tiff', img)[1]).decode('utf-8') ##check
+    else:
+        return base64.b64encode(image_path.read_bytes()).decode('utf-8')
 
 def save_to_json(filename, evalResult, output_dir, category, subcategory):
 
-    # Create a dictionary to store the metadata
     metadata = {
         "fileName": filename,
         "evaluationResult": evalResult
     }
 
-    # Save the metadata to a JSON file
     output_path = os.path.join(output_dir, category, subcategory, f"{os.path.basename(os.path.splitext(image_path)[0])}.json")
     with open(output_path, 'w') as f:
         json.dump(metadata, f, indent=4)
